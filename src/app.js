3c34cb6d66455700215aee46d2b3918aba9524e7
tom.chen <tom603172002@gmail.com>
2017-06-01 11:35:02 +0800

    add redux-logger middleware

--- src/app.js
+++ src/app.js
@@ -8,21 +8,29 @@ ReactDOM.render(
   document.getElementById('app')); */
 
 
-import { createStore } from 'redux'
+import { applyMiddleware,createStore } from 'redux'
 import * as types from './actions/actionType'
 import routeReducers from './reducers/routeReducers'
+import { createLogger } from 'redux-logger'
 
+
+//use redux-logger
+const logger = createLogger();
+
+//combine middleware
+const middleware = applyMiddleware(logger);
 
 //建立 store 
 //第一個參數為 reducer , 第二個為初始值
-const store = createStore(routeReducers, {});
+const store = createStore(routeReducers, {},middleware);
 
 //當 state 改變,就更新 UI
 store.subscribe(()=>{
+    console.log('subscribe state:');
     console.log(store.getState());
 })
 
-//UI 發 action
+//UI(button) 發 action
 store.dispatch({type:types.INC,payload:1});
 store.dispatch({type:types.INC,payload:1});
 store.dispatch({type:types.INC,payload:1});

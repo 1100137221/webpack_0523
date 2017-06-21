a00f96c4c5e88f69a53d213720813b6d3f603c6f
tom.chen <tom603172002@gmail.com>
2017-06-21 18:10:02 +0800

    建立 rwd 首頁

--- src/containers/App.js
+++ src/containers/App.js
@@ -5,9 +5,10 @@ import thunk  from 'redux-thunk'
 import { applyMiddleware,createStore } from 'redux'
 import routeReducers from '../reducers/routeReducers'
 import {Provider} from 'react-redux';
-import Login from '../login/login';
 
 
+import '../scss/thirth.scss'
+import '../scss/app.scss'
 
 //use redux-logger
 const logger = createLogger();
@@ -25,10 +26,44 @@ const store = createStore(routeReducers, {},middleware);
 class App extends Component {
     render() {
         return (
-            <div>
-                test123456
-                <Login test='123'></Login>
+            <div id="hero">
+                <div className="container">
+                    <div className="row">
+                        <nav className="navbar navbar-toggleable-md navbar-inverse">
+                            <a href="#" className="navbar-brand text-primary" id="logo">GAMELOGO</a>
 
+                            <button className="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                                <span className="navbar-toggler-icon"></span>
+                            </button>
+
+                            <div className="collapse navbar-collapse justify-content-end" id="navbarSupportedContent">
+                                <ul className="navbar-nav">
+                                    <li className="nav-item">
+                                        <a className="nav-link text-primary" href="#">Overview</a>
+                                    </li>
+                                    <li className="nav-item">
+                                        <a className="nav-link text-primary" href="#">Specs</a>
+                                    </li>
+                                    <li className="nav-item">
+                                        <a className="nav-link text-primary" href="#">FAQ</a>
+                                    </li>
+                                    <li className="nav-item">
+                                        <a className="nav-link text-primary" href="#">Purchase</a>
+                                    </li>
+                                </ul>
+                            </div>
+                        </nav>
+                    </div>
+                    <div className="row hero-content pt-lg-6 pb-lg-6 pt-5">
+                        <div className='col-md-6 col-sm-12'>
+                            <h1 className='text-uppercase text-center text-md-left'>Destroy All Of Humanity</h1>
+                            <h1 className='text-uppercase text-center text-md-left'>Capture enmy territory add best theme down like rodent.</h1>
+                        </div>
+                        <div className='col-md-6 col-sm-12'>
+                            <img src={require('../assets/overlay.png')} alt="" className='hero-img mt-5 mt-md-0'/>
+                        </div>
+                    </div>
+                </div>
             </div>
         );
     }
@@ -37,7 +72,6 @@ class App extends Component {
 App.propTypes = {
 
 };
-console.log($);
 
 render(
      <App />,
